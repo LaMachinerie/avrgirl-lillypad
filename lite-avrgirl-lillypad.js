ba8e167ec90cfb45096d69c07aae2af5afae0cf7
JulesTopart <jules.topart@gmail.com>
2019-03-19 12:34:31 +0100

    Update lite-avrgirl-lillypad.js

--- lite-avrgirl-lillypad.js
+++ lite-avrgirl-lillypad.js
@@ -12,7 +12,7 @@ var AvrgirlArduino = function(opts) {
 
   this.options = {
     debug: opts.debug || false,
-    board: opts.board || 'uno',
+    board: opts.board || 'lillypad-usb',
     port: opts.port || '',
     manualReset: opts.manualReset || false
   };
